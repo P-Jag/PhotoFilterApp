31e2f7e6e1ae251b455048126b8ad42c538ff003
Pawel Jagla <pavel.jagla@gmail.com>
2022-01-03 17:23:35 +0100

    initial

---
+++ CameraRxSwiftPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraRxSwiftPractice
+//
+//  Created by Pawel Jagla on 03/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
