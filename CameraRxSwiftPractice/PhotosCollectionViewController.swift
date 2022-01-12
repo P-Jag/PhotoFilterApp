0674c41f4881bbe720aa77f87dfc7c9759082d4b
Pawel Jagla <pavel.jagla@gmail.com>
2022-01-12 21:06:26 +0100

    storyboard layout fix

--- CameraRxSwiftPractice/PhotosCollectionViewController.swift
+++ CameraRxSwiftPractice/PhotosCollectionViewController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { status in
+            
+            if status == .authorized {
+                
+            }
+        }
     }
 }
